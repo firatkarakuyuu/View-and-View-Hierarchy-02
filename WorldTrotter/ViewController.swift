8b211e0797bb62b17879770fefbe5855ae6ee17c
defaultprofile <19062738@g200029-19f.local>
2019-09-27 14:54:26 -0400

    Lab Initial #5 to Rest of All

--- WorldTrotter/ViewController.swift
+++ WorldTrotter/ViewController.swift
@@ -15,24 +15,24 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        //madde 2
-        
-        firstView.frame = CGRect(x: 150, y: 150, width: 100, height: 100)
-        firstView.backgroundColor = UIColor.blue
-        self.view.addSubview(firstView)
-        
-        
-        //madde 3
-        
-        secondView.frame = CGRect(x: 50, y:50, width:75, height:75)
-        secondView.backgroundColor = UIColor.green
-        self.view.addSubview(secondView)
-        
-        //madde 4
-        
-        secondView.removeFromSuperview()
-        firstView.addSubview(secondView)
+//
+//        //madde 2
+//
+//        firstView.frame = CGRect(x: 150, y: 150, width: 100, height: 100)
+//        firstView.backgroundColor = UIColor.blue
+//        self.view.addSubview(firstView)
+//
+//
+//        //madde 3
+//        
+//        secondView.frame = CGRect(x: 50, y:50, width:75, height:75)
+//        secondView.backgroundColor = UIColor.green
+//        self.view.addSubview(secondView)
+//
+//        //madde 4
+//
+//        secondView.removeFromSuperview()
+//        firstView.addSubview(secondView)
         
     }
     
